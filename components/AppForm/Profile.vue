2bc4e18fb4b07c84d19e1ccfcea6c065144d39ba
tylerdipolito <tyler@jaksapparel.com>
2024-08-26 19:27:08 -0400

    edit

--- components/AppForm/Profile.vue
+++ components/AppForm/Profile.vue
@@ -4,10 +4,10 @@
     description="Some public information about you"
   >
     <div class="shadow sm:overflow-hidden sm:rounded-md">
-      <div class="space-y-6 bg-white px-4 py-5 sm:p-6">
-
+      <div class="grid grid-cols-1 sm:grid-cols-2 gap-6 bg-white px-4 py-5 sm:p-6">
+        
         <!-- Name Field -->
-        <div class="col-span-6 sm:col-span-3">
+        <div class="col-span-1 sm:col-span-1">
           <label for="name" class="block text-lg font-bold text-gray-800">Name</label>
           <input
             type="text"
@@ -19,26 +19,9 @@
             class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm text-gray-900"
           />
         </div>
-
-        <!-- About Field -->
-        <div>
-          <label for="about" class="block text-lg font-bold text-gray-800">About yourself</label>
-          <div class="mt-1">
-            <textarea
-              id="about"
-              name="about"
-              rows="3"
-              class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm text-gray-900 placeholder-gray-500"
-              placeholder="I am an astronaut"
-              maxlength="100"
-              :value="desc"
-              @input="$emit('update:desc', $event.target.value)"
-            ></textarea>
-          </div>
-        </div>
-
+        
         <!-- Photo URL Field -->
-        <div class="flex-grow col-span-6 sm:col-span-3">
+        <div class="col-span-1 sm:col-span-1">
           <label for="photo-url" class="block text-lg font-bold text-gray-800">Photo URL</label>
           <input
             type="text"
@@ -49,9 +32,26 @@
             @input="$emit('update:image', $event.target.value)"
           />
         </div>
+        
+        <!-- About Field -->
+        <div class="col-span-1 sm:col-span-2">
+          <label for="about" class="block text-lg font-bold text-gray-800">About yourself</label>
+          <div class="mt-1">
+            <textarea
+              id="about"
+              name="about"
+              rows="4"
+              class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm text-gray-900 placeholder-gray-500"
+              placeholder="I am an astronaut"
+              maxlength="100"
+              :value="desc"
+              @input="$emit('update:desc', $event.target.value)"
+            ></textarea>
+          </div>
+        </div>
 
         <!-- Email Address Field -->
-        <div class="flex-grow col-span-6 sm:col-span-3">
+        <div class="col-span-1 sm:col-span-2">
           <label for="email" class="block text-lg font-bold text-gray-800">Email Address</label>
           <input
             type="email"
